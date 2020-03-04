130daefe208a6e4a1ca95684d20a858d2015b83e
Ryan Filler <ryanfiller89@gmail.com>
2020-03-03 21:55:15 -0600

    set gatsby colors, add body color, fix table width

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,16 +1,16 @@
 const colors = {
-  colorPrimary: '#192368',
-  colorDark: '#1e053f',
-  colorLight: '#fff7ff',
-  colorHighlight: '#7c1863',
-  colorActive: '#ed6554',
-  colorDisabled: '#2f323a'
-  // colorDark: '#0f0f0f',
-  // colorLight: '#ffffff',
-  // colorPrimary: 'rebeccapurple',
+  // colorPrimary: '#192368',
+  // colorDark: '#1e053f',
+  // colorLight: '#fff7ff',
+  // colorHighlight: '#7c1863',
+  // colorActive: '#ed6554',
+  // colorDisabled: '#2f323a'
+  colorDark: '#0f0f0f',
+  colorLight: '#ffffff',
+  colorPrimary: 'rebeccapurple',
   // colorActive: 'rebeccapurple',
   // colorHighlight: 'rebeccapurple',
-  // colorDisabled: '#78757a'
+  colorDisabled: '#78757a'
 }
 
 const cssVars = Object.entries(colors).map(color => {
